5a74f5e6f137ffd222b1d0d8014c0091dd33058f
rajeshsohani <78219018+rajeshsohani@users.noreply.github.com>
2024-09-22 06:38:58 +0530

    Update admin.py

--- Tutorial/admin.py
+++ Tutorial/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import *
-#from django_summernote.widgets import SummernoteWidget
+from django_summernote.widgets import SummernoteWidget
 from django.db import models
 
 
