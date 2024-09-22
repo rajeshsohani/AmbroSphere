e2f322a082c529d4f7bbfefeb06443f17b5bf79b
rajeshsohani <78219018+rajeshsohani@users.noreply.github.com>
2024-09-22 06:32:15 +0530

    Update admin.py

--- Tutorial/admin.py
+++ Tutorial/admin.py
@@ -3,30 +3,30 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import *
-from django_summernote.widgets import SummernoteWidget
+#from django_summernote.widgets import SummernoteWidget
 from django.db import models
 
 
 class SubjectAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.TextField: {'widget': SummernoteWidget},
-    }
+   # formfield_overrides = {
+  #      models.TextField: {'widget': SummernoteWidget},
+ #   }
 
 admin.site.register(SubjectModel,SubjectAdmin)
 
 
 
 class TopicAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.TextField: {'widget': SummernoteWidget},
-    }
+    #formfield_overrides = {
+     #   models.TextField: {'widget': SummernoteWidget},
+    #}
 admin.site.register(TopicModel,TopicAdmin)
 
 
 class SubtopiAdmin(admin.ModelAdmin):
-    formfield_overrides = {
-        models.TextField: {'widget': SummernoteWidget},
-    }
+    #formfield_overrides = {
+     #   models.TextField: {'widget': SummernoteWidget},
+    #}
 
 admin.site.register(SubtopicModel,SubtopiAdmin)
 
